5921d89b5838c257cd244201e612d1db6b59e6f3
mdlu <42189754+mdlu@users.noreply.github.com>
2019-07-28 12:11:09 +0300

    fixed part 2 tests

--- part2-test.py
+++ part2-test.py
@@ -25,10 +25,9 @@ else:
 
 ## Test 4. Tests the decode with product of 2 primes
 print("Test 3")
-if(part2.encode(66839) in [(751,89),(89,751)]):
+if(part2.decode(66839) in [(751,89),(89,751)]):
 	print("PASSED")
 else:
 	print("FAILED - for the input of 66839, decode should return (751,89) in some order")
-, decode 
 
 ##Write more test cases here
